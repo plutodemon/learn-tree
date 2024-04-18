58a119e789eb101a6eabe071b07edca8d847be6c
lemonshuo <101779730+plutodemon@users.noreply.github.com>
2024-04-18 18:59:12 +0800

    update: md & code

--- python/demo_01/learn_1.py
+++ python/demo_01/learn_1.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 a = b = 1
 c, d = 2, 3
@@ -42,3 +43,55 @@ print("\a")  # 响铃
 print("\000")  # 空字符
 print("\v")  # 垂直制表符
 print("\t")  # 水平制表符
+
+# 条件判断
+if c < b:
+    print("1 < 2")
+elif a < b:
+    print("1 == 2")
+# switch case
+match c:
+    case 1:
+        print("1")
+    case _:
+        print("default")
+# 循环
+while c < 10:
+    c += 1
+    print(c)
+else:
+    print("end")
+
+for i in range(10):
+    pass
+    print(i)
+else:
+    print("end")
+
+# 列表推导式
+names = ['Bob', 'Tom', 'alice', 'Jerry', 'Wendy', 'Smith']
+new_names = [name.upper() for name in names if len(name) > 3]
+print(new_names)
+
+# 字典推导式
+listDemo = ['Google', 'Runoob', 'Taobao']
+newDict = {key: len(key) for key in listDemo}
+dic = {x: x ** 2 for x in (2, 4, 6)}
+
+# 元组推导式 生成器表达式
+a = (x for x in range(1, 10))
+
+# 迭代器
+listD = [1, 2, 3, 4]
+it = iter(listD)  # 创建迭代器对象
+
+for x in it:
+    print(x, end=" ")
+# 或者
+while True:
+    try:
+        print(next(it))
+    except StopIteration:
+        sys.exit()
+
+# 生成器  https://www.runoob.com/python3/python3-iterator-generator.html
