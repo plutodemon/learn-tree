dbd91f59eefda80856dd887561a56e9458d36262
lemonshuo <101779730+plutodemon@users.noreply.github.com>
2024-05-15 19:01:18 +0800

    update: md & question_34

--- golang/leetcode/question_34.go
+++ golang/leetcode/question_34.go
@@ -2,19 +2,27 @@ package leetcode
 
 func searchRange(nums []int, target int) []int {
 	left, right := 0, len(nums)-1
-	mid := len(nums) / 2
-	for left < right {
-		if nums[mid] == target {
-
-		} else if nums[mid] < target {
-			left = mid
+	for left <= right {
+		mid := (left + right) / 2
+		if nums[mid] >= target {
+			right = mid - 1
 		} else {
-			right = mid
+			left = mid + 1
 		}
 	}
-
-	if left > right {
+	if left == len(nums) || nums[left] != target {
 		return []int{-1, -1}
 	}
-	return []int{left, right}
+	start := left
+	left, right = 0, len(nums)-1
+	for left <= right {
+		mid := (left + right) / 2
+		if nums[mid] >= target+1 {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	end := left - 1
+	return []int{start, end}
 }
