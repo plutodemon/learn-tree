c4bfa1c54e4668f75cedf22082afb0c9cf8e7fbd
lemonshuo <101779730+plutodemon@users.noreply.github.com>
2024-05-16 18:48:13 +0800

    update: md & question_2529

--- golang/leetcode/question_34.go
+++ golang/leetcode/question_34.go
@@ -1,5 +1,6 @@
 package leetcode
 
+// question_34
 func searchRange(nums []int, target int) []int {
 	left, right := 0, len(nums)-1
 	for left <= right {
@@ -25,4 +26,23 @@ func searchRange(nums []int, target int) []int {
 	}
 	end := left - 1
 	return []int{start, end}
+}
+
+// question_2529
+func maximumCount(nums []int) int {
+	small, large := 0, 0
+	for _, num := range nums {
+		if num == 0 {
+			continue
+		}
+		if num > 0 {
+			large++
+			continue
+		}
+		small++
+	}
+	if small > large {
+		return small
+	}
+	return large
 }
