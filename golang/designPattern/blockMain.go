bf372b7c6c50caf3d5fa0c8f95a8128326c54c61
lemonshuo <101779730+plutodemon@users.noreply.github.com>
2024-07-09 18:54:55 +0800

    update: 两协程交替打印数字

--- golang/designPattern/blockMain.go
+++ golang/designPattern/blockMain.go
@@ -60,3 +60,33 @@ func demo2() {
 		time.Sleep(1 * time.Second)
 	}
 }
+
+func demo3(n int) {
+	c1 := make(chan bool, 1)
+	c2 := make(chan bool, 1)
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	go func(n int) {
+		defer wg.Done()
+		for i := 2; i <= n; i += 2 {
+			<-c1
+			fmt.Println(i)
+			c2 <- true
+		}
+	}(n)
+
+	go func(n int) {
+		defer wg.Done()
+		for i := 1; i <= n; i += 2 {
+			<-c2
+			fmt.Println(i)
+			c1 <- true
+		}
+	}(n)
+
+	// 启动第一个goroutine，因为从1开始，所以先启动打印奇数的协程
+	c2 <- true
+
+	wg.Wait()
+}
