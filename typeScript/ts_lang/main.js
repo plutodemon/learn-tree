4222c3ae96da18f0fbbe0d04c70b907590fabdb7
lemonshuo <101779730+plutodemon@users.noreply.github.com>
2024-07-29 12:00:13 +0800

    update: ts config

--- typeScript/ts_lang/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function greeter(person) {
-    if (person === "Jane User") {
-        return "Hello, Jane User";
-    }
-    return "Hello, " + person;
-}
-let user = "Jane";
-console.log(greeter(user));
