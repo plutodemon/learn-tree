e933397cb587c88c6f5e5bad7c1a9872b3fc096d
lemonshuo <101779730+plutodemon@users.noreply.github.com>
2024-07-30 17:50:07 +0800

    update: ts config & webpack config

--- typeScript/README.md
+++ typeScript/README.md
@@ -1,4 +1,15 @@
 ## 撒点儿调料——TypeScript
 
 ### PS:
+
 - 公司项目需要, 暂定需要用ts写脚本, 写一些机器人, 本项目作为学习ts的一个记录
+
+## 使用webpack打包ts
+
+- 初始化package.json (打包配置文件) `npm init -y`
+- 安装webpack `npm i -D webpack webpack-cli typescript ts-loader`
+- 编写webpack配置文件 [webpack.config.js](webpack.config.js)
+- 编写ts配置文件 [tsconfig.json](tsconfig.json)
+- 修改[package.json](package.json)添加打包命令
+    - `"dev": "webpack --mode development",`
+    - `"build": "webpack --mode production"`
---
+++ typeScript/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+// webpack中的所有配置信息都应该写在module.exports中
+module.exports = {
+    // 指定入口文件
+    entry: './src/index.ts',
+    // 指定输出文件
+    output: {
+        // 指定输出文件的目录
+        path: path.resolve(__dirname, 'dist'),
+        // 打包后输出文件的文件名
+        filename: 'bundle.js'
+    },
+    // 设置mode development为开发模式 production为生产模式
+    mode: 'development',
+    // 指定webpack打包时要使用的模块
+    module: {
+        // 指定加载的规则
+        rules: [
+            {
+                // test指定的是规则生效的文件
+                test: /\.ts$/,
+                // 要使用的loader
+                use: 'ts-loader',
+                // 要排除的文件
+                exclude: /node_modules/
+            }
+        ]
+    },
+
+};
