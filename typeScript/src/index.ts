8b923a0d64577f35728a5f51c3402599e368d6f5
lemonshuo <101779730+plutodemon@users.noreply.github.com>
2024-08-05 19:01:46 +0800

    update: ts demo

--- typeScript/src/index.ts
+++ typeScript/src/index.ts
@@ -2,13 +2,13 @@
 
 class Person {
     // 实例属性
-    name: string;
+    // private name: string;
     age: number;
     // 静态属性(类属性)
     static readonly type = 'Person';
 
     // 构造函数
-    constructor(name: string, age: number) {
+    constructor(private name: string, age: number) {
         this.name = name;
         this.age = age;
     }
@@ -16,10 +16,20 @@ class Person {
     say() {
         console.log(`name: ${this.name}, age: ${this.age}`);
     }
+
+    set setName(name: string) {
+        this.name = name;
+    }
+
+    get getName() {
+        return this.name;
+    }
 }
 
 const p = new Person('Tom', 188);
 p.say();
+p.setName = 'Jerry';
+console.log(p.getName);
 
 // 类的继承
 class Student extends Person {
@@ -34,9 +44,60 @@ class Student extends Person {
 
     // 方法的重写
     say() {
-        console.log(`${this.name} is studying...`);
+        console.log(`${this.getName} is studying...`);
     }
 }
 
 const s = new Student('Jerry', 20, 1001);
 s.say();
+
+// 抽象类 不能被实例化(不能new) 只能被继承
+abstract class Animal {
+    // 抽象方法 只能定义在抽象类中 不能有具体实现 必须在子类中实现
+    abstract say(): void;
+}
+
+// 接口 用于定义对象的结构 限制类的结构
+interface PersonInterface {
+    name: string;
+    age: number;
+
+    say(): void;
+}
+
+interface PersonInterface {
+    sex: string;
+}
+
+class MyPerson implements PersonInterface {
+    name: string;
+    age: number;
+
+    say(): void {
+        throw new Error("Method not implemented.");
+    }
+
+    sex: string;
+
+    constructor(name: string, age: number, sex: string) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+}
+
+// 泛型
+interface Inter {
+    length: number;
+}
+
+function createArray<T extends Inter>(value: T): T[] {
+    const arr = [];
+    for (let i = 0; i < 10; i++) {
+        arr[i] = value;
+    }
+    return arr;
+}
+
+createArray('a')
+//createArray<number>(1)
